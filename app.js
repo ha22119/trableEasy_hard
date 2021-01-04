9c9d94ac95f808c127e8bdfb0635c29047f648aa
ha22119 <55187236+ha22119@users.noreply.github.com>
2021-01-04 21:55:16 +0900

    app.js_infoRouter 추가
               -오늘의 교훈-
    
    같은 코드를 작업할때는 main에서 하는게 오류가 날 확률이 적다

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var signupRouter = require('./routes/signup');
 var mainRouter = require('./routes/main');
-
+var infoRouter = require('./routes/information');
 
 var app = express();
 
@@ -28,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signup', signupRouter);
 app.use('/main', mainRouter);
+app.use('/info', infoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
