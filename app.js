2c5868cec819dab7aa2635a2efbc4ea5467ac55a
111111022 <51100712+111111022@users.noreply.github.com>
2021-01-04 21:03:32 +0900

    로그인화면에서 로고를 클릭하면 메인화면으로 이동합니다
    임시로 한거니까 로그인 기능 되면 바로 없애겟음

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var signupRouter = require('./routes/signup');
+var mainRouter = require('./routes/main');
+
 
 var app = express();
 
@@ -25,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signup', signupRouter);
+app.use('/main', mainRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/main.js
+++ routes/main.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/main', function(req, res, next) {
-  res.render('main', { title: 'Express' });
-});
-
 module.exports = router;
