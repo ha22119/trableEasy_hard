26c8653e0427410de27c33163462cf66a0a769aa
ha22119 <55187236+ha22119@users.noreply.github.com>
2021-01-04 21:53:03 +0900

    test2
    오류 해결용

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signup', signupRouter);
-app.use('/info', infoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
