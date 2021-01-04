a41f865934cc5609c77051b7ebb65f35f5f5d385
ha22119 <55187236+ha22119@users.noreply.github.com>
2021-01-04 21:44:25 +0900

    information_프론트 완료
    여행 코스 정보 보기

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var signupRouter = require('./routes/signup');
+var infoRouter = require('./routes/information');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signup', signupRouter);
+app.use('/info', infoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
