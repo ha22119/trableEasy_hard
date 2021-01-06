a69a03c1cd31627c13d1e41261336e8803b43aa9
111111022 <51100712+111111022@users.noreply.github.com>
2021-01-06 17:31:43 +0900

    여행코스 추천

--- routes/main.js
+++ routes/main.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/info', function(req, res, next) {
+  res.render('information', { title: 'Express' });
+});
+
 module.exports = router;
